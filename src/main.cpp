643599ade2cf33d4e0e11a4c810feeba18bb9637
qqqq <fmitb3334@gmail.com>
2026-04-06 23:16:03 +0300

    Better drawing

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,52 @@
 #include <filesystem>
 #include <iostream>
+#include <string>
 
 namespace fs = std::filesystem;
 
-auto run(fs::path def) {
+auto run(fs::path def) -> void {
     std::cout << ("\033[2J\033[H");
+    int depth = 0;
+    std::string filename{def};
     for (fs::recursive_directory_iterator it{def}, end; it != end; it++) {
         if (it->path() == it->path().parent_path() / ".git") {
             it.disable_recursion_pending();
             continue;
         }
 
-        for (int i = 0; i < it.depth(); ++i) {
-            std::cout << "│    ";
+        for (int i = 0; i < depth; i++) {
+            std::cout << "│   ";
         }
 
-        if (it->is_directory()) {
-            std::cout << "├───" + it->path().stem().string() << std::endl;
-        } else {
-            std::cout << "├───" + it->path().filename().string() << std::endl;
+        if (filename == def) {
+            std::cout << filename << std::endl;
+            filename = it->path().filename().string();
+            depth = it.depth();
+            continue;
         }
+
+        if (depth > it.depth()) {
+            std::cout << "└───" + filename << std::endl;
+        } else {
+            std::cout << "├───" + filename << std::endl;
+        }
+
+        filename = it->path().filename().string();
+        depth = it.depth();
     }
+    for (int i = 0; i < depth; i++) {
+        std::cout << ".   ";
+    }
+    std::cout << "└───" + filename << std::endl;
 }
 
-auto main() -> int {
+auto main(int argc, char* argv[]) -> int {
     fs::path def = ".";
+    if (argc > 1) {
+        def = argv[1];
+    }
     run(def);
+
     while (1) {
         if (std::cin.get()) {
             run(def);
@@ -34,10 +55,11 @@ auto main() -> int {
     return 0;
 }
 
-// Super minimal (and ugly)
+// Super minimal
 // TODO
+// ?[-] Storing data
+// ?[-] Sorting (by name? by type?)
+// [?] Proper drawing
 // [-] Auto update
-// [-] Storing data
-// [-] Sorting (by name? by type?)
-// [-] Proper drawing
+// [-] Better input handling
 // [-] Ignore options (.gitignore and "." files)
